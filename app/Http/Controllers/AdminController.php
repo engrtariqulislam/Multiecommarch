38e182c8c310a4be21a642d8a26f487fc0f4b54a
Tariqul islam <tnawayn83@yahoo.com>
2022-10-18 23:59:57 +0300

    Admin Logout Option & login page
    Admin Logout Option & login page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class AdminController extends Controller
 {
     public function Admindashboard(){
@@ -11,4 +13,22 @@ class AdminController extends Controller
         return view('admin.index');
 
     } // End Mehtod
+
+    
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    } // End Mehtod
+
+    public function AdminDestroy(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    } // End Mehtod 
+
+    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,12 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('admin/dashboard',[AdminController::class,'Admindashboard'])->name('admin.dashboard');
-    
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+
+
 });
+
+
 
 
 // Vendor Dashbord
@@ -39,3 +43,7 @@ Route::middleware(['auth','role:vendor'])->group(function(){
 Route::get('vendor/dashboard',[VendorController::class,'Vendordashboard'])->name('vendor.dashboard');
 
 });
+
+
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
