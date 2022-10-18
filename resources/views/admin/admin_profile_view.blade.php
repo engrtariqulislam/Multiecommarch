d520a29c63d109aef08ffd957d89fb4833accccf
Tariqul islam <tnawayn83@yahoo.com>
2022-10-19 01:25:50 +0300

    Admin Profile & Image Update Part 4
    Admin Profile & Image Update Part 4

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -55,7 +55,8 @@
 	<div class="card">
 		<div class="card-body">
 
-		<form method="post" action="#" enctype="multipart/form-data" >
+            <form method="post" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data" >
+                @csrf
 			
 		
 			<div class="row mb-3">
