eec9adec571c24b86ad8e4a4523237457d5f3857
Tariqul islam <tnawayn83@yahoo.com>
2022-10-21 00:17:37 +0300

    User Logout Option
    User Logout Option

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,6 +46,14 @@ class UserController extends Controller
         return redirect()->back()->with($notification);
 
     } // End Mehtod 
+    public function UserLogout(Request $request){
+        Auth::guard('web')->logout();
 
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } // End Mehtod 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::middleware(['auth'])->group(function() {
 
     Route::get('/dashboard', [UserController::class, 'UserDashboard'])->name('dashboard');
     Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
+    Route::get('/user/logout', [UserController::class, 'UserLogout'])->name('user.logout');
+    
 });   // Gorup Milldeware End
 
 
