ab66dff23efa6b064475f517be8587a210bf8717
Tariqul islam <tnawayn83@yahoo.com>
2022-10-22 01:16:05 +0300

    Subcategory Crud Part 1
    Subcategory Crud Part 1

---
+++ app/Http/Controllers/Backend/SubcategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\SubCategory;
+
+class SubcategoryController extends Controller
+{
+    public function AllSubCategory(){
+        $subcategories = SubCategory::latest()->get();
+        return view('backend.subcategory.subcategory_all',compact('subcategories'));
+    } // End Method 
+
+    
+}
