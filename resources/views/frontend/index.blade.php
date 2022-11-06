b966e6d7504af913a693ef85301b28d77dc22baf
Tariqul islam <tnawayn83@yahoo.com>
2022-11-06 21:37:47 +0300

    112.Hot Deals & Special Offer Product Show
    Hot Deals & Special Offer Product Show

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -505,6 +505,95 @@ $discount = ($amount/$product->selling_price) * 100;
  </section>
  <!--End Fashion Category -->
 
+<!--Hot deals -->
+<section class="section-padding mb-30">
+    <div class="container">
+        <div class="row">
+ <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 wow animate__animated animate__fadeInUp" data-wow-delay="0">
+    <h4 class="section-title style-1 mb-30 animated animated"> Hot Deals </h4>
+    <div class="product-list-small animated animated">
+
+        @foreach($hot_deals as $item)                   
+        <article class="row align-items-center hover-up">
+            <figure class="col-md-4 mb-0">
+                <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
+            </figure>
+            <div class="col-md-8 mb-0">
+                <h6>
+                    <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"> {{ $item->product_name }} </a>
+                </h6>
+                <div class="product-rate-cover">
+                    <div class="product-rate d-inline-block">
+                        <div class="product-rating" style="width: 90%"></div>
+                    </div>
+                    <span class="font-small ml-5 text-muted"> (4.0)</span>
+                </div>
+                 @if($item->discount_price == NULL)
+                         <div class="product-price">
+                            <span>${{ $item->selling_price }}</span>
+    
+                        </div>
+    
+                        @else
+                        <div class="product-price">
+                            <span>${{ $item->discount_price }}</span>
+                            <span class="old-price">${{ $item->selling_price }}</span>
+                        </div>
+                        @endif
+            </div>
+        </article>
+        @endforeach
+    
+    
+    
+                            </div>
+                      </div>
+                 <!--Hot deals end -->       
+
+            <!-- Special Offer -->   
+                 <div class="col-xl-3 col-lg-4 col-md-6 mb-md-0 wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
+                    <h4 class="section-title style-1 mb-30 animated animated">  Special Offer </h4>
+                    <div class="product-list-small animated animated">
+
+                        @foreach($special_offer as $item)                   
+                        <article class="row align-items-center hover-up">
+                            <figure class="col-md-4 mb-0">
+                                <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
+                            </figure>
+                            <div class="col-md-8 mb-0">
+                                <h6>
+                                    <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"> {{ $item->product_name }} </a>
+                                </h6>
+                                <div class="product-rate-cover">
+                                    <div class="product-rate d-inline-block">
+                                        <div class="product-rating" style="width: 90%"></div>
+                                    </div>
+                                    <span class="font-small ml-5 text-muted"> (4.0)</span>
+                                </div>
+                                 @if($item->discount_price == NULL)
+                                         <div class="product-price">
+                                            <span>${{ $item->selling_price }}</span>
+                    
+                                        </div>
+                    
+                                        @else
+                                        <div class="product-price">
+                                            <span>${{ $item->discount_price }}</span>
+                                            <span class="old-price">${{ $item->selling_price }}</span>
+                                        </div>
+                                        @endif
+                            </div>
+                        </article>
+                        @endforeach
+                    
+                    
+                    
+                                            </div>
+                                        </div>
+
+                        
+
+                        <!--Special Offer end -->
 
  @include('frontend.home.home_vendor_list')
 
