a96a12c9be0d239828c7c2d382eb261799cb3a7d
Tariqul islam <tnawayn83@yahoo.com>
2022-11-15 00:00:22 +0300

    146.Compare Product Option Setup Part 1
    Compare Product Option Setup Part 1

--- resources/views/frontend/home/home_features_product.blade.php
+++ resources/views/frontend/home/home_features_product.blade.php
@@ -40,7 +40,7 @@ $featured = App\Models\Product::where('featured',1)->orderBy('id','DESC')->limit
                     
                     <a aria-label="Add To Wishlist" class="action-btn" id="{{ $product->id }}" onclick="addToWishList(this.id)"  ><i class="fi-rs-heart"></i></a>
                     
-   <a aria-label="Compare" class="action-btn"  id="{{ $product->id }}" onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
+                    <a aria-label="Compare" class="action-btn"  id="{{ $product->id }}" onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
 
    <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal" data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)" ><i class="fi-rs-eye"></i></a>
                 </div>
