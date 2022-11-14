ae097eedae849570532368b662cf0ef4d3ac8524
Tariqul islam <tnawayn83@yahoo.com>
2022-11-14 23:53:37 +0300

    145.Wishlist Product Remove With Ajax
    Wishlist Product Remove With Ajax

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -103,9 +103,9 @@
             </div>
 
             <div class="header-action-icon-2">
-                <a href="shop-wishlist.html">
+                <a href="{{ route('wishlist') }}">
                     <img class="svgInject" alt="Nest" src="{{ asset('frontend/assets/imgs/theme/icons/icon-heart.svg') }}" />
-                    <span class="pro-count blue">6</span>
+                    <span class="pro-count blue" id="wishQty"></span>
                 </a>
                 <a href="{{ route('wishlist') }}"><span class="lable">Wishlist</span></a>
             </div>
