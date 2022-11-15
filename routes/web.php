43cd9809051a57999c6d8a8e56b9e8ff96270ab2
Tariqul islam <tnawayn83@yahoo.com>
2022-11-15 21:01:51 +0300

    160.Admin Panel Shipping Area Division Create Part 2
    Admin Panel Shipping Area Division Create Part 2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\BannerController;
 use App\Http\Controllers\Backend\CouponController;
+use App\Http\Controllers\Backend\ShippingAreaController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\User\WishlistController;
@@ -220,6 +221,17 @@ Route::controller(CouponController::class)->group(function(){
     Route::get('/edit/coupon/{id}' , 'Editcoupon')->name('edit.coupon');
     Route::post('/update/coupon' , 'Updatecoupon')->name('update.coupon');
     Route::get('/delete/coupon/{id}' , 'Deletecoupon')->name('delete.coupon');
+
+}); 
+
+ // Shipping Division All Route 
+   Route::controller(ShippingAreaController::class)->group(function(){
+    Route::get('/all/division' , 'AllDivision')->name('all.division');
+    Route::get('/add/division' , 'AddDivision')->name('add.division');
+    Route::post('/store/division' , 'StoreDivision')->name('store.division');
+    Route::get('/edit/coupon/{id}' , 'EditCoupon')->name('edit.coupon');
+    Route::post('/update/coupon' , 'UpdateCoupon')->name('update.coupon');
+    Route::get('/delete/coupon/{id}' , 'DeleteCoupon')->name('delete.coupon');
 
 }); 
    
